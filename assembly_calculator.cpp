e728e4de202856d217db5777a70c0508764c592f
Gasegamer <gabrielciricarvalho@gmail.com>
2025-06-07 18:06:58 -0300

    Funcionamento dos botões de número.
    Foi adicionado o código para o funcionamento dos botões de número (0 à 9) além do funcionamento do botão de apagar (C).
    
    O design da calculadora foi refatorado para incluir a função de fatorial e o design do display foi melhorado.

--- assembly_calculator.cpp
+++ assembly_calculator.cpp
@@ -1,14 +1,93 @@
 #include "assembly_calculator.h"
 #include "./ui_assembly_calculator.h"
+#include "assembly_operations.h"
+
 
 Assembly_Calculator::Assembly_Calculator(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::Assembly_Calculator)
 {
     ui->setupUi(this);
+
+    this->valorAtual = 0.0;
+
+    ui->expressao->setText(QString::number(this->valorAtual));
+    /*
+     *  Adicionando um validador.
+     *  Ele permitirá que sejam usados apenas
+     *  números e operadores.
+     */
+    QRegularExpression rx("[0-9+\\-*/().^!\\s]+");
+
+    QValidator * validador = new QRegularExpressionValidator(rx, this);
+
+    ui->expressao->setValidator(validador);
+
+    /*
+     * Inicializando os botões de número.
+    */
+    QPushButton * botoesNumero[BOTOES_NUMERO] = {
+        ui->num_0, ui->num_1, ui->num_2, ui->num_3, ui->num_4,
+        ui->num_5, ui->num_6, ui->num_7, ui->num_8, ui->num_9
+    };
+
+    for (QPushButton* botao : botoesNumero) {
+        connect(botao, &QPushButton::clicked, this, &Assembly_Calculator::numeroPressionado);
+    }
 }
 
 Assembly_Calculator::~Assembly_Calculator()
 {
     delete ui;
 }
+
+void Assembly_Calculator::numeroPressionado()
+{
+    QPushButton * botao = (QPushButton *)sender();
+
+    QString digito = botao->text(); // Lê o número do botão.
+
+    QString novoTexto = ui->expressao->text() + digito;
+
+    double novoValor = novoTexto.toDouble();
+
+    this->valorAtual = novoValor;
+
+    ui->expressao->setText(QString::number(this->valorAtual));
+}
+
+void Assembly_Calculator::on_apagar_clicked()
+{
+    ui->expressao->clear();
+
+    this->valorAtual = 0.0;
+
+    ui->expressao->setText(QString::number(this->valorAtual));
+}
+
+/*
+ * Posiciona o cursor sempre no final do display.
+*/
+void Assembly_Calculator::on_expressao_cursorPositionChanged(int arg1, int arg2)
+{
+    if (ui->expressao->hasFocus())
+    {
+        ui->expressao->setCursorPosition(ui->expressao->text().length());
+    }
+}
+
+void Assembly_Calculator::on_expressao_textEdited(const QString &texto)
+{
+    if(texto.isEmpty())
+    {
+        this->valorAtual = 0.0;
+
+        ui->expressao->setText(QString::number(this->valorAtual));
+    }
+    else
+    {
+        this->valorAtual = texto.toDouble();
+        ui->expressao->setText(QString::number(this->valorAtual));
+    }
+}
+
--- assembly_calculator.h
+++ assembly_calculator.h
@@ -2,6 +2,10 @@
 #define ASSEMBLY_CALCULATOR_H
 
 #include <QMainWindow>
+#include <QRegularExpressionValidator>
+#include <QValidator>
+
+#define BOTOES_NUMERO 10
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +21,18 @@ public:
     Assembly_Calculator(QWidget *parent = nullptr);
     ~Assembly_Calculator();
 
+private slots:
+    void numeroPressionado();
+
+    void on_apagar_clicked();
+
+    void on_expressao_cursorPositionChanged(int arg1, int arg2);
+
+    void on_expressao_textEdited(const QString &texto);
+
 private:
     Ui::Assembly_Calculator *ui;
+
+    double valorAtual;
 };
 #endif // ASSEMBLY_CALCULATOR_H
